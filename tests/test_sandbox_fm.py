6a99c9b60f818b4dc5eb50760845bbf2565aa0c6
Fedor Baart <fedor.baart@deltares.nl>
2016-10-12 00:48:14 +0200

    testing

--- tests/test_sandbox_fm.py
+++ tests/test_sandbox_fm.py
@@ -10,11 +10,23 @@ Tests for `sandbox_fm` module.
 
 import pytest
 
+import logging
+
 from contextlib import contextmanager
 from click.testing import CliRunner
 
 from sandbox_fm import sandbox_fm
 from sandbox_fm import cli
+
+logger = logging.getLogger(__name__)
+
+
+def log_ex(ex):
+    try:
+        raise ex
+    except:
+        logger.exception("exception raised")
+
 
 
 class TestSandbox_fm(object):
@@ -28,13 +40,19 @@ class TestSandbox_fm(object):
 
     def test_command_line_interface(self):
         runner = CliRunner()
+
+        # run with test input
         result = runner.invoke(cli.main, [
             'tests/depthimage.png',
             'tests/zandmotor/zm_tide.mdu',
             '--max-iterations=1'
         ])
-        assert result.exit_code == 0, (result.output, result)
+        logger.info(result.output)
+        log_ex(result.exception)
+        assert result.exit_code == 0, (result)
         assert 'sandbox_fm.cli.main' in result.output
+
+        # run help
         help_result = runner.invoke(cli.main, ['--help'])
         assert help_result.exit_code == 0
         assert '--help  Show this message and exit.' in help_result.output
