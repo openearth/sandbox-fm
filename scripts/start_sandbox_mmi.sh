a5d7777bc2aaba04ab998422d56a1d08024e7004
Sandbox <jurjen.dejong@deltares.nl>
2018-05-18 21:17:28 +0200

    Startup scripts

--- scripts/start_sandbox_mmi.sh
+++ scripts/start_sandbox_mmi.sh
@@ -6,8 +6,13 @@ source ~/Envs/sandbox/bin/activate
 model=../models/Lent/FlowFM.mdu
 
 cd ~/sandbox-fm/scripts/
+echo 'in sandbox folder' > autostart.log
 
-sandbox-fm run $model &
+mmi-runner dflowfm $model --port 62000 --pause -o s1 -o bl -o ucx -o ucy -o zk &
+
+sleep 5
+
+sandbox-fm run --mmi tcp://localhost:62000  $model &
 
 sleep 5
 
@@ -19,5 +24,6 @@ wmctrl -r 'Sandbox_figure' -e 0,1920,0,640,480
 wmctrl -r 'Sandbox_figure' -b toggle,fullscreen
 wmctrl -r 'BPS' -b toggle,fullscreen
 
+echo 'done' > autostart2.log
 
 read
