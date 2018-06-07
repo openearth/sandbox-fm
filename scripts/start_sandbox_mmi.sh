86c1c683f94a7d3424f8eaec35802dfc73c07e05
DIRECTORY\jong_jn <jurjen.dejong@deltares.nl>
2018-06-07 18:44:35 +0200

    Colormap can now be adjusted in the config.json

--- scripts/start_sandbox_mmi.sh
+++ scripts/start_sandbox_mmi.sh
@@ -3,11 +3,9 @@ MPLBACKEND=qt5agg
 
 source ~/Envs/sandbox/bin/activate
 
-model=../models/Lent/FlowFM.mdu
+model=../models/Waal_schematic/waal_with_side.mdu
 
 cd ~/sandbox-fm/scripts/
-echo 'in sandbox folder' > autostart.log
-
 mmi-runner dflowfm $model --port 62000 --pause -o s1 -o bl -o ucx -o ucy -o zk &
 
 sleep 5
@@ -16,13 +14,13 @@ sandbox-fm run --mmi tcp://localhost:62000  $model &
 
 sleep 5
 
-bps.py &
+# bps.py &
 
 sleep 5
 
-wmctrl -r 'Sandbox_figure' -e 0,1920,0,640,480
-wmctrl -r 'Sandbox_figure' -b toggle,fullscreen
-wmctrl -r 'BPS' -b toggle,fullscreen
+# wmctrl -r 'Sandbox_figure' -e 0,1920,0,640,480
+# wmctrl -r 'Sandbox_figure' -b toggle,fullscreen
+# wmctrl -r 'BPS' -b toggle,fullscreen
 
 echo 'done' > autostart2.log
 
