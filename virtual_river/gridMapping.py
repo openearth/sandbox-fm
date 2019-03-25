cab86bf09c544e50f7ef83114eeba79a096f1e3c
Robert-Jan den Haan <r.j.denhaan@gmail.com>
2019-03-25 11:50:52 +0100

    Updated gridMapping
    New version of the gridMapping script including an interpolation based on triangulation to the nearest three hexagons.

--- virtual_river/gridMapping.py
+++ virtual_river/gridMapping.py
@@ -8,9 +8,12 @@ import json
 import cv2
 import geojson
 import numpy as np
-from scipy import interpolate
-import rtree
-import shapely.geometry
+#from scipy import interpolate
+from scipy import spatial
+#import rtree
+from shapely import geometry
+from shapely.ops import unary_union
+import time
 import netCDF4
 import bmi.wrapper
 
@@ -48,90 +51,123 @@ def read_grid():
     feature_collection = geojson.FeatureCollection(features)
     with open('grid.geojson', 'w') as f:
         geojson.dump(feature_collection, f, sort_keys=True, indent=2)
-    return feature_collection, x, y
+    return feature_collection, xy
 
 
-def hex_to_points(hexagons, grid, xi, yi, method='nearest'):
-    idx = rtree.index.Index()
+def hex_to_points(hexagons, grid, xy, method='nearest'):
+    hex_coor = []
+    polygons = []
     for feature in hexagons.features:
-        shape = shapely.geometry.asShape(feature.geometry)
-        idx.insert(feature.id, shape.bounds, obj=feature.properties)
+        shape = geometry.asShape(feature.geometry)
+        x_hex = shape.centroid.x
+        y_hex = shape.centroid.y
+        hex_coor.append([x_hex, y_hex])
+        polygons.append(shape)
+    hex_coor = np.array(hex_coor)
+    hex_locations = spatial.cKDTree(hex_coor)
+    multipolygon = geometry.MultiPolygon(polygons)
+    board_as_polygon = unary_union(multipolygon)
+    board_shapely = geometry.mapping(board_as_polygon)
+    board_feature = geojson.Feature(geometry=board_shapely)
+    board_featurecollection = geojson.FeatureCollection([board_feature])
+    with open('board_border.geojson', 'w') as f:
+        geojson.dump(board_featurecollection, f, sort_keys=True, indent=2)
+    inside_id = []
+    inside_coor = []
+    for feature in grid.features:
+        point = geometry.asShape(feature.geometry)
+        if board_as_polygon.contains(point):
+            feature.properties["board"] = True
+            inside_id.append(feature.id)
+            x_point = point.centroid.x
+            y_point = point.centroid.y
+            inside_coor.append([x_point, y_point])
+        else:
+            feature.properties["board"] = False
 
-    model2hex = {}
+    inside_coor = np.array(inside_coor)
+    inside_locations = spatial.cKDTree(inside_coor)
+    nearest = {}
+    distances = {}
     hexagons_by_id = {feature.id: feature for feature in hexagons.features}
 
     for feature in grid.features:
-        # find nearest cell
-        # store in feature
-        shape = shapely.geometry.asShape(feature.geometry)
-        nearest = next(idx.nearest(shape.bounds, objects=True))
-        model2hex[feature.id] = nearest.id
-
+        shape = geometry.asShape(feature.geometry)
+        x_hex = shape.centroid.x
+        y_hex = shape.centroid.y
+        xy = np.array([x_hex, y_hex])
+        if feature.properties["board"]:
+            dist, indices = hex_locations.query(xy, k=3)
+            nearest[feature.id] = indices.tolist()
+            distances[feature.id] = dist.tolist()
+        else:
+            dist, indices = inside_locations.query(xy)
+            nearest[feature.id] = indices
+            distances[feature.id] = dist
+            """
+            change this section to finding the nearest neighbour on the horizontal axis +
+            another rule if no nearest neighbour on the horizontal axis
+            """
+    tac = time.time()
     for feature in grid.features:
-        hexagon = hexagons_by_id[model2hex[feature.id]]
-        feature.properties['z'] = hexagon.properties['z']
-
-    if method == 'griddata':
-        x = []
-        y = []
-        z = []
-        for feature in hexagons['features']:
-            shape = shapely.geometry.asShape(feature.geometry)
-            x_hex = int(shape.centroid.x)
-            y_hex = int(shape.centroid.y)
-            z_hex = feature.properties["z"]
-            x.append(x_hex)
-            y.append(y_hex)
-            z.append(z_hex)
-        x = np.array(x)
-        y = np.array(y)
-        z = np.array(z)
-
-        z_interpolate = interpolate.griddata((x, y), z, (xi, yi), method='linear')
-        #z_extrapolate = interpolate.griddata((xi,yi),z_interpolate,(xi,yi),method='nearest')
-        #z_nearest = interpolate.griddata((x,y),z,(xi,yi),method='linear')
-
-        for i, feature in enumerate(grid.features):
-            if np.isnan(z_interpolate[i]):
-                continue
+        if feature.properties["board"]:
+            nearest_three = nearest[feature.id]
+            distances2hex = distances[feature.id]
+            if distances2hex[1] > 45:
+                hexagon = hexagons_by_id[nearest_three[0]]
+                feature.properties['z'] = hexagon.properties['z']
+            elif distances2hex[2] > 45:
+                hexagon1 = hexagons_by_id[nearest_three[0]]
+                hexagon2 = hexagons_by_id[nearest_three[1]]
+                distances2hex = np.power(distances2hex, 3)
+                dist2hex1 = 1 / distances2hex[0]
+                dist2hex2 = 1 / distances2hex[1]
+                total_dist = dist2hex1 + dist2hex2
+                feature.properties['z'] = hexagon1.properties['z'] * (dist2hex1 / total_dist) + hexagon2.properties['z'] * (dist2hex2 / total_dist)
             else:
-                feature.properties['z'] = z_interpolate[i]
+                hexagon1 = hexagons_by_id[nearest_three[0]]
+                hexagon2 = hexagons_by_id[nearest_three[1]]
+                hexagon3 = hexagons_by_id[nearest_three[2]]
+                distances2hex = np.power(distances2hex, 3)
+                dist2hex1 = 1 / distances2hex[0]
+                dist2hex2 = 1 / distances2hex[1]
+                dist2hex3 = 1 / distances2hex[2]
+                total_dist = dist2hex1 + dist2hex2 + dist2hex3
+                feature.properties['z'] = hexagon1.properties['z'] * (dist2hex1 / total_dist) + hexagon2.properties['z'] * (dist2hex2 / total_dist) + hexagon3.properties['z'] * (dist2hex3 / total_dist)
+        else:
+            continue
 
-    with open('grid_with_z.geojson', 'w') as f:
-        geojson.dump(grid, f, sort_keys=True, indent=2)
-
-    """
+    grid_by_id = {feature.id: feature for feature in grid.features}
     for feature in grid.features:
-        # find nearest cell
-        # store in feature
-        shape = shapely.geometry.asShape(feature.geometry)
-        nearest = next(idx.nearest(shape.bounds, objects=True))
-        # nearest = next(idx.nearest(shape.bounds, objects=True))
-        # model2hex[feature.id] = nearest.id
-        # print('no interpolation method developed yet')
-    else:
-        for feature in grid.features:
-            # find nearest cell
-            # store in feature
-            shape = shapely.geometry.asShape(feature.geometry)
-            nearest = next(idx.nearest(shape.bounds, objects=True))
-            model2hex[feature.id] = nearest.id
-        """
-    """
-    for feature in grid.features:
-        hexagon = hexagons_by_id[model2hex[feature.id]]
-        feature.properties['z'] = hexagon.properties['z']
+        if not feature.properties["board"]:
+            inside_point = grid_by_id[inside_id[nearest[feature.id]]]
+            feature.properties['z'] = inside_point.properties['z']
+        else:
+            continue
 
-    with open('grid_with_z.geojson', 'w') as f:
-        geojson.dump(grid, f, sort_keys=True, indent=2)
     """
+    Optie:
+        - binnen bord True boolean
+        - buiten bord False boolean
+        - True --> nearest three hexagons (huidige implementatie)
+        - False --> zoek nearest horizontale neighbour die True is
+        - Geen nearest neighbour horizontaal --> nearest hexagon
+    """
+
+    with open('grid_with_z_triangulate.geojson', 'w') as f:
+        geojson.dump(grid, f, sort_keys=True, indent=2)
+    return tac
 
 
 if __name__ == "__main__":
+    tic = time.time()
     calibration = read_calibration()
     hexagons = read_hexagons()
-    grid, xi, yi = read_grid()
-    hex_to_points(hexagons, grid, xi, yi, method='griddata')
+    grid, xy = read_grid()
+    tac = hex_to_points(hexagons, grid, xy, method='griddata')
     model = bmi.wrapper.BMIWrapper('dflowfm')
     model.initialize(r'C:\Users\HaanRJ\Documents\GitHub\sandbox-fm\models\sandbox\Waal_schematic\waal_with_side.mdu')
     print('model initialized')
+    toc = time.time()
+    print('loading time:', tac-tic)
+    print('interpolation time:', toc-tac)
